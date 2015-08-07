a1a7250420223a54b646d537b68b1c3d630eeb4e
Petermuturi <pmuturi65@gmail.com>
2015-08-07 18:56:43 +0300

    added log in  and register user

---
+++ app/src/main/java/com/alienske/msconnect/GetUserCallBack.java
@@ -0,0 +1,7 @@
+package com.alienske.msconnect;
+
+/**
+ * Created by Main_Hub on 8/7/2015.
+ */
+public class GetUserCallBack {
+}
---
+++ app/src/main/java/com/alienske/msconnect/UserLocalStore.java
@@ -0,0 +1,7 @@
+package com.alienske.msconnect;
+
+/**
+ * Created by Main_Hub on 8/6/2015.
+ */
+public class UserLocalStore {
+}
